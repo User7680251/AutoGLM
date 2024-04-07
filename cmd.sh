146775e40b005a72191e254fe130e99da8a7f491
User7680251 <2591592360@qq.com>
2024-04-07 18:20:49 +0800

    240407

--- cmd.sh
+++ cmd.sh
@@ -1,15 +1,3 @@
-# # 验证模型 demo
-# SAT_HOME=/root/.sat_models 
-# export HF_ENDPOINT=https://hf-mirror.com
-# python cli_demo.py 
-
-# # 微调模型 demo
-# SAT_HOME=/root/.sat_models 
-# bash finetune/finetune_visualglm.sh
-
-# # 验证微调后模型 demo
-# python cli_demo.py --from_pretrained /root/VisualGLM-6B/checkpoints/finetune-visualglm-6b-04-05-13-58 --prompt_zh 这张图片的背景里有什么内容？
-
 # # 微调模型 data
 # SAT_HOME=/root/.sat_models 
 # bash finetune/finetune_visualglm.sh
@@ -18,15 +6,9 @@
 # export HF_ENDPOINT=https://hf-mirror.com
 # python cli_demo.py --from_pretrained /home/AutoGLM/checkpoints/ptuning_CODA1500 --prompt_zh 任务：图片中为汽车在行驶,请观察路况并输出按照给定格式输出驾驶动作和推理过程。
 
-# # GUI 
-# SAT_HOME=/root/.sat_models 
-# unset http_proxy
-# unset https_proxy
-# python web_demo.py --share
-
 # api
 export HF_ENDPOINT=https://hf-mirror.com
 python api.py --from_pretrained /home/AutoGLM/checkpoints/lora_CODA100_1
 
 # streamlit web ui 
-streamlit run utils/webui.py --server.address 0.0.0.0
+streamlit run utils/webui.py --server.address 0.0.0.0 --server.enableXsrfProtection=false
--- utils/webui.py
+++ utils/webui.py
@@ -12,27 +12,33 @@ def image_to_base64(image_file):
 # FastAPI应用的URL
 url = "http://127.0.0.1:8080"
 
-st.title("图片上传和API调用演示")
+st.title("AutoGLM")
 
 # 创建一个文件上传组件
-uploaded_file = st.file_uploader("上传图片", type=["jpg", "png"])
+uploaded_file = st.file_uploader("上传行车图片", type=["jpg", "png"])
 
 if uploaded_file is not None:
     # 显示上传的图片
-    st.image(uploaded_file, caption='上传的图片', use_column_width=True)
+    st.image(uploaded_file, caption='', use_column_width=True)
 
     # 将图片编码为Base64
     image_encoded = image_to_base64(uploaded_file)
 
     # 构造请求的数据
     data = {
-        "text": "任务：图片中为汽车在行驶，请观察路况并输出按照给定格式输出驾驶动作和推理过程。规则：在输出驾驶动作时，仅输出一种动作，使用以下标识符{}进行标记：紧急制动使用{EM} ，减速观察使用{DS} ，正常行驶使用{NS}。推理过程请使用一个[]标记。举例：{EM}[前方路面有障碍物，需要立即进行紧急制动，以避免碰撞]。输出：",
+        "text": "任务：图片中为汽车在行驶,请观察路况并输出按照给定格式输出驾驶动作和推理过程。",
         "image": image_encoded,
         "history": None
     }
 
+    # 禁用代理
+    proxies = {
+        "http": None,
+        "https": None,
+    }
+
     # 发送POST请求
-    response = requests.post(url, json=data)
+    response = requests.post(url, json=data, proxies=proxies)
 
     # 确保响应状态码是200
     if response.status_code == 200:
@@ -43,10 +49,10 @@ if uploaded_file is not None:
         result = parsed_json.get('result', None)
 
         # 显示结果
-        st.write("API返回的结果:")
+        st.write("AutoGLM返回结果:")
         st.write(result)
     else:
         st.write("Error: Server returned status code", response.status_code)
 
 # 运行Streamlit应用
-# 在命令行中运行以下命令：streamlit run app.py
+# 在命令行中运行以下命令：streamlit run utils/webui.py --server.address 0.0.0.0 --server.enableXsrfProtection=false
