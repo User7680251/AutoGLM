6be8d060bb2ad86d4533b52df850d4231cd5412e
User7680251 <2591592360@qq.com>
2024-05-03 12:30:03 +0800

    240503

--- metrics/metrics.sh
+++ metrics/metrics.sh
@@ -1,8 +1,8 @@
 export HF_ENDPOINT=https://hf-mirror.com
 
 json_path="data/json/2_reason_answer/test.json"
-pretrained_model="/home/AutoGLM/checkpoints/2"
-save_name="2"
+pretrained_model="/home/AutoGLM/checkpoints/2_1"
+save_name="2_1"
 
 python /home/AutoGLM/api.py --from_pretrained $pretrained_model & api_pid=$!
 sleep 30
